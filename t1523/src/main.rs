8f12c4252541b40e52e63f6ce7c84c4557d0d88c
TnZzZHlp <tnzzzhlp@outlook.com>
2025-05-26 10:42:48 +0800

    添加 t1523 项目的 count_odds 函数实现，用于计算指定范围内的奇数数量。

--- t1523/src/main.rs
+++ t1523/src/main.rs
@@ -1,3 +1,15 @@
+impl Solution {
+    pub fn count_odds(low: i32, high: i32) -> i32 {
+        let mut result = 0;
+        for i in low..high + 1 {
+            if i % 2 != 0 {
+                result += 1
+            }
+        }
+        result
+    }
+}
+
 struct Solution {}
 
 fn main() {}
